c9cc2724fb95c1a0080e30af789ff01f3db14bd3
Gennady Zaicev <zaicev.gennady@yandex.ru>
2022-01-12 18:31:10 +0300

    пуш

--- frontend/src/component/StationSlider/StationSlider.jsx
+++ frontend/src/component/StationSlider/StationSlider.jsx
@@ -7,8 +7,38 @@ import { LazyImageWrapper } from "../LazyImage/LazyImage";
 import SecondaryButton from "../uikit/SecondaryButton/SecondaryButton";
 import ArrowSectionButton from "../uikit/ArrowSectionButton/ArrowSectionButton";
 import AboutMoreButton from "../uikit/AboutMoreButton/AboutMoreButton";
+
+const dataImages = [
+  {
+    src: "furshet_slider/desktop/1.jpg",
+    srcTablet: "furshet_slider/tablet/1.jpg",
+    srcMobile: "furshet_slider/mobile/1.jpg",
+  },
+  {
+    src: "furshet_slider/desktop/2.jpg",
+    srcTablet: "furshet_slider/tablet/2.jpg",
+    srcMobile: "furshet_slider/mobile/2.jpg",
+  },
+  {
+    src: "furshet_slider/desktop/3.jpg",
+    srcTablet: "furshet_slider/tablet/3.jpg",
+    srcMobile: "furshet_slider/mobile/3.jpg",
+  },
+  {
+    src: "furshet_slider/desktop/4.jpg",
+    srcTablet: "furshet_slider/tablet/4.jpg",
+    srcMobile: "furshet_slider/mobile/4.jpg",
+  },
+];
+
+const dataText = {
+  header: "Фуршет с обслуживанием",
+  title:
+    "Создаем фуршетную линию, полностью организуем ваше мероприятие с накрытием на стол, декором и официантами",
+};
+
 // TODO: Добавить динамическую загрузку в этот слайдер и сделать проверку на редирект
-export default function StationSlider({ data, secondBtn }) {
+export default function StationSlider({ dataText, secondBtn }) {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [loaded, setLoaded] = useState(false);
   const [sliderRef, instanceRef] = useKeenSlider({
@@ -28,25 +58,31 @@ export default function StationSlider({ data, secondBtn }) {
     <>
       <div className={cs("navigation-wrapper", s.slider_wrapper)}>
         <div ref={sliderRef} className={cs("keen-slider", s.slider_block)}>
-          <div className={cs("keen-slider__slide", s.slide)}>
-            <LazyImageWrapper
-              src="/images/banner-min.jpg"
-              className={[s.slide_image]}
-              wrapperClass={s.wrapper_image}
-            />
-            <div className={s.content}>
-              <h2 className={s.head}>Фуршет с обслуживанием</h2>
-              <p className={s.description}>
-                Создаем фуршетную линию, полностью организуем ваше мероприятие с
-                накрытием на стол, декором и официантами
-              </p>
-              {secondBtn ? (
-                <SecondaryButton text={"Свяжитесь со мной"} />
-              ) : (
-                <AboutMoreButton />
-              )}
-            </div>
+          <div className={s.content}>
+            <h2 className={s.head}>Фуршет с обслуживанием</h2>
+            <p className={s.description}>
+              Создаем фуршетную линию, полностью организуем ваше мероприятие с
+              накрытием на стол, декором и официантами
+            </p>
+            {secondBtn ? (
+              <SecondaryButton text={"Свяжитесь со мной"} />
+            ) : (
+              <AboutMoreButton />
+            )}
           </div>
+          {dataImages.map((item, index) => {
+            return (
+              <div key={index} className={cs("keen-slider__slide", s.slide)}>
+                <LazyImageWrapper
+                  src={item.src}
+                  srcTablet={item.srcTablet}
+                  srcMobile={item.srcMobile}
+                  className={[s.slide_image]}
+                  wrapperClass={s.wrapper_image}
+                />
+              </div>
+            );
+          })}
         </div>
         {loaded && instanceRef.current && (
           <div className={s.arrows}>
